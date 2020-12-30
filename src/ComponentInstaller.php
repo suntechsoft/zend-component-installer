82364ae63e533051d4310d177bbb273083d53ef1
Paul Kamensky <p.kamensky@globalgames.net>
2020-12-30 11:06:13 +0200

    Composer 2 fix

--- src/ComponentInstaller.php
+++ src/ComponentInstaller.php
@@ -835,4 +835,14 @@ class ComponentInstaller implements
 
         return [];
     }
+
+    public function deactivate(Composer $composer, IOInterface $io)
+    {
+        // Not needed
+    }
+
+    public function uninstall(Composer $composer, IOInterface $io)
+    {
+        // Not needed
+    }
 }
